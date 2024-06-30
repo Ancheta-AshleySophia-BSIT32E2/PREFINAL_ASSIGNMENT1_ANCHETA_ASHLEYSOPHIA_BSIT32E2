7b74937a5cde4407431bd917c27110afa382703b
Ancheta-AshleySophia-BSIT32E2 <161100971+Ancheta-AshleySophia-BSIT32E2@users.noreply.github.com>
2024-06-30 19:06:46 +0800

    PREFINALS ASSIGNMENRT #1

---
+++ PREFINALSAUTH_ANCHETA_ASHLEY_SOPHIA/ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            
+            var userInfo = new
+            {
+                Name = "Ashley Sophia S. Ancheta",
+                Section = "BSIT 32E2",
+                Course = "Bachelor of Science in Infortmation Technology"
+            };
+
+            var funFacts = new[]
+            {
+                "I hate insects!",
+                "I enjoy playing video games especially CODM and Mobile Legends.",
+                "I love watching series and movies.",
+            };
+
+            return Ok(new { UserInfo = userInfo, FunFacts = funFacts });
+        }
+    }
+}
